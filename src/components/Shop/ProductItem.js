e1ff1d41fd3f4aa310def44eea44abe67c2027e0
Pankaj10101 <rkay1048@gmail.com>
2023-05-20 11:42:19 +0530

    add and remove item

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,9 +1,18 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
-
+  const dispatch = useDispatch();
+  const { title, price, description, id } = props;
+  const addToCart = () => {
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price
+    }));
+  };
   return (
     <li className={classes.item}>
       <Card>
@@ -12,8 +21,8 @@ const ProductItem = (props) => {
           <div className={classes.price}>${price.toFixed(2)}</div>
         </header>
         <p>{description}</p>
-        <div className={classes.actions}>
-          <button>Add to Cart</button>
+        <div className={classes.actions}> 
+          <button onClick={addToCart}>Add to Cart</button>
         </div>
       </Card>
     </li>
